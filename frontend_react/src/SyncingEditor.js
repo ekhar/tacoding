beb5ba117de4e7bcfe5612d525ddd39c607d1be7
Eric Kharitonashvili <ericsrealemail@gmail.com>
2021-09-14 16:49:46 -0400

    working with multiple tabs of chrome. Not diff computers.

--- frontend_react/src/SyncingEditor.js
+++ frontend_react/src/SyncingEditor.js
@@ -2,33 +2,39 @@
 import React, { useMemo, useRef, useState, useEffect } from "react";
 // Import the Slate editor factory.
 import { createEditor } from "slate";
-import Mitt from "mitt";
 
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from "slate-react";
 import { initialvalue } from "./slateinitialvalue";
 
-const emitter = new Mitt();
+var socket = new WebSocket("ws://localhost:8000/ws");
+
 export const SyncingEditor = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
   // Add the initial value when setting up our state.
   const [value, setValue] = useState(initialvalue);
   const id = useRef(String(Date.now()));
   const remote = useRef(false);
-  const editor_ref = useRef(editor);
 
   useEffect(() => {
-    emitter.on("*", (type, ops) => {
-      console.log("emmiter running");
-      if (id.current !== type) {
-        remote.current = true;
-        console.log(ops);
-        ops.forEach((op) => {
-          editor.apply(op);
-        });
-        remote.current = false;
-      }
-    });
+    socket.addEventListener(
+      "message",
+      (data) => {
+        var x = JSON.parse(JSON.parse(data.data).body);
+        let editor_id = x.editor_id;
+        let ops = x.ops;
+
+        console.log("running");
+        if (id.current !== editor_id) {
+          remote.current = true;
+          JSON.parse(ops).forEach((op) => {
+            editor.apply(op);
+          });
+          remote.current = false;
+        }
+      },
+      { once: true }
+    );
   });
 
   return (
@@ -37,7 +43,8 @@ export const SyncingEditor = () => {
       value={value}
       onChange={(newValue) => {
         setValue(newValue);
-        const ops = editor_ref.current.operations
+
+        const ops = editor.operations
           .filter((o) => {
             if (o) {
               return (
@@ -50,9 +57,12 @@ export const SyncingEditor = () => {
           })
           .map((o) => ({ ...o, data: { source: "one" } }));
         if (ops.length && !remote.current) {
-          console.log("OP are this long" + ops.length);
-          emitter.emit(id.current, ops);
-          emitter.all.clear();
+          socket.send(
+            JSON.stringify({
+              editor_id: id.current,
+              ops: JSON.stringify(ops),
+            })
+          );
         }
       }}
     >
