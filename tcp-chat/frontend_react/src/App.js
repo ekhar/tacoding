6bd12be9dcb383a98bf5b6f8002fffe47cf56661
Eric Kharitonashvili <ericsrealemail@gmail.com>
2021-09-14 10:42:20 -0400

    first commit

---
+++ tcp-chat/frontend_react/src/App.js
@@ -0,0 +1,13 @@
+// Import React dependencies.
+import { SyncingEditor } from "./SyncingEditor";
+
+const App = () => {
+  return (
+    <div>
+      <SyncingEditor />
+      <br />
+      <SyncingEditor />
+    </div>
+  );
+};
+export default App;
---
+++ tcp-chat/frontend_react/src/slateinitialvalue.js
@@ -0,0 +1,6 @@
+export const initialvalue = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
